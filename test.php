34d6f2513be183f6c2b400e625cfd3d46778d38f
PracaOl <oliwer.pietrzycki@op.pl>
2022-11-09 15:11:59 +0100

    NewChangesPartOne

--- test.php
+++ test.php
@@ -1,15 +1,20 @@
 <?php
+$fajnyhtml = $_REQUEST['fajnyhtml'];
 
-$db = new mysqli('localhost', 'root', '', 'debug_test');
+include("headerhtml.html");
+include("$fajnyhtml.html");
+include("footerhtml.html");
 
-$id = 1;
-$q = $db->prepare("SELECT * FROM test WHERE id = ?");
-$q->bind_param('i', $id);
-if ($q->execute()) {
-    $result = $q->get_result();
-    $row = $result->fetch_assoc();
+//$db = new mysqli('localhost', 'root', '', 'debug_test');
+
+//$id = 1;
+//$q = $db->prepare("SELECT * FROM test WHERE id = ?");
+//$q->bind_param('i', $id);
+//if ($q->execute()) {
+    //$result = $q->get_result();
+    //$row = $result->fetch_assoc();
     //eeeeeaeaeaeaea
 
-}
+//}
 
 ?>
