692db591b259b5d4852f930b28f8191292f98caf
PracaOl <oliwer.pietrzycki@op.pl>
2022-11-16 15:14:26 +0100

    Fixes - By Accident Set First Name Twice.

--- register.php
+++ register.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Logowanie</title>
+    <title>Rejestrowanie</title>
 </head>
 <body>
     <form action="" method="post">
@@ -24,7 +24,7 @@
         require_once('class/user.class.php');
         $user = new User ($_REQUEST['login'], $_REQUEST['password']);
         $user->setFirstName($_REQUEST['firstName']);
-        $user->setFirstName($_REQUEST['lastName']);
+        $user->setLastName($_REQUEST['lastName']);
         if ($user->register()) {
             echo "Zarejestrowano poprawnie";
         } else {
