644ce43a9df36973f06e29dba8b797e633b88bd1
PracaOl <oliwer.pietrzycki@op.pl>
2022-11-23 13:21:24 +0100

    Testing Twig

--- config.php
+++ config.php
@@ -1,4 +1,9 @@
 <?php
-    $db = new mysqli('localhost', 'root', '', 'loginFormOP');
+require_once('vendor/autoload.php');
+
+$loader = new Twig\Loader\FilesystemLoader('templates');
+$twig = new Twig\Environment($loader);
+
+$db = new mysqli('localhost', 'root', '', 'loginFormOP');
 
 ?>
